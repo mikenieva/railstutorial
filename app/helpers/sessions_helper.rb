61f4ece7b7728e318bb9a099c27465595af85167
Miguel Nieva <miguelnieva@conexionew.com>
2013-12-26 20:44:53 -0600

    Finish sign in

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,34 @@
+module SessionsHelper
+	def sign_in(user)
+		# Primero, creamos el nuevo token
+		remember_token = User.new_remember_token
+		# Colocamos el token sin encriptar en los cookies del navegador
+		cookies.permanent[:remember_token] = remember_token
+		# Salvamos el token encriptado a la base de datos
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+		# Establecemos al usuario actual con el usuario que se generó
+		self.current_user = user
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		remember_token = User.encrypt(cookies[:remember_token])
+		@current_user ||= User.find_by(remember_token: remember_token)
+	end
+
+	def sign_out
+		# Cambiamos el token dle usuario en la Base de Datos
+		current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
+		# Usamos el método DELETE sobre las cookies para quitar el token de la sesión
+		cookies.delete(:remember_token)
+		# Establecemos el usuario recurrente a nil
+		self.current_user = nil
+	end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ end
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -97,6 +99,11 @@ end
     specify { expect(user_for_invalid_password).to be_false }
   end
 
+end
+
+describe "remember token" do
+  before { @user.save }
+  its(:remember_token) { should_not be_blank }
 end
 
 end
